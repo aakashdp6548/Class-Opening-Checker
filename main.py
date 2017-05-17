52c8379f5ab7fbee1c9933d4afde957f5edd6d98
Aakash Patel <aakashdp@umich.edu>
2017-05-17 01:17:15 -0400

    code will run every two hours when executed

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
 from schedule_api import *
+from datetime import datetime
+import time
+import os
+import logging
+import sys
+from termcolor import cprint, colored
+from apscheduler.schedulers.background import BackgroundScheduler
+
+section_info = {    "term_code": "2160",
+                    "school_code": "ENG",
+                    "subject_code": "EECS",
+                    "catalog_num": "280",
+                    "section_type": "a"
+                }
 
 def get_open_seats(section_list, section_type):
     open_classes = {}
 
     for section in section_list:
-        if section["SectionType"] == section_type:
+        if section_info["section_type"] == "a":
+            if section["AvailableSeats"] > 0:
+                open_classes[section["SectionNumber"]] = section["AvailableSeats"]
+
+        elif section["SectionType"] == section_info["section_type"]:
             if section["AvailableSeats"] > 0:
                 open_classes[section["SectionNumber"]] = section["AvailableSeats"]
 
     return open_classes
 
-term_code = "2160"
-school_code = "ENG"
-subject_code = "EECS"
-catalog_num = "280"
+def print_open_sections():
 
-sections = get_sections(term_code, school_code, subject_code, catalog_num)
-open_sections = get_open_seats(sections, "DIS")
+    sections = get_sections("2160", "ENG", "EECS", "280")
+    open_sections = get_open_seats(sections, section_info["section_type"])
 
-for section in open_sections:
-    print section, ": ", open_sections[section]
+    if len(open_sections) != 0:
+        print time.strftime("%H:%M:%S")
+        for section in open_sections:
+            print "Section " + str(section) + ": " + str(open_sections[section])
+    else:
+        print time.strftime("%H:%M:%S"), "No open sections found"
+
+if __name__ == '__main__':
+
+    print "Checking openings for %s %s %s %s (%s)" % ( section_info["term_code"], section_info["school_code"], section_info["subject_code"], section_info["catalog_num"], section_info["section_type"] )
+    logging.basicConfig()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(print_open_sections, 'interval', hours=2)
+    scheduler.start()
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
